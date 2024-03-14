d97a59c6a8682aaab3bda64966d5a8863a4cf420
Charles JAVERLIAT <charles.javerliat@gmail.com>
2024-03-14 23:14:43 +0100

    Update export_xdf test

--- tests/test_export_xdf.py
+++ tests/test_export_xdf.py
@@ -1,9 +1,12 @@
 import pytest
 
 from plume_python.export.xdf_exporter import export_xdf_from_record
+from pathlib import Path
 
 
 def test_export_xdf():
     record = pytest.record
+    # create directory tests/test_outputs if it does not exist
+    Path('tests/test_outputs').mkdir(parents=True, exist_ok=True)
     with open('tests/test_outputs/test.xdf', 'wb') as f:
         export_xdf_from_record(f, record)
