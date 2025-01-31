7a411364e8fd7e870b6f1a0b8dc0f9a057a0ddef
Javerliat Charles <charles.javerliat@gmail.com>
2025-01-31 15:48:17 +0100

    Create a new decoder on every frames, markers, signals or inputs calls.

--- plume_python/decoder/record_decoder.py
+++ plume_python/decoder/record_decoder.py
@@ -8,34 +8,46 @@ from plume_python.decoder.marker_decoder import MarkerDecoder
 from plume_python.decoder.lsl_stream_decoder import LslStreamDecoder
 from plume_python.decoder.input_action_decoder import InputActionDecoder
 
-from typing import Iterator
+from typing import Generator
 
 
 class RecordDecoder:
+    
     def __init__(self, filepath: str):
-        self._frame_decoder = FrameDecoder(filepath)
-        self._marker_decoder = MarkerDecoder(filepath)
-        self._signal_decoder = LslStreamDecoder(filepath)
-        self._input_action_decoder = InputActionDecoder(filepath)
-
-    def close(self):
-        self._frame_decoder.close()
-        self._marker_decoder.close()
-        self._signal_decoder.close()
-        self._input_action_decoder.close()
+        self.filepath = filepath
 
     @property
-    def frames(self) -> Iterator[Frame]:
-        return self._frame_decoder
+    def frames(self) -> Generator[Frame, None, None]:
+        frame_decoder = FrameDecoder(self.filepath)
+        try:
+            for frame in frame_decoder:
+                yield frame
+        finally:
+            frame_decoder.close()
 
     @property
-    def markers(self) -> Iterator[Marker]:
-        return self._marker_decoder
+    def markers(self) -> Generator[Frame, None, None]:
+        marker_decoder = MarkerDecoder(self.filepath)
+        try:
+            for marker in marker_decoder:
+                yield marker
+        finally:
+            marker_decoder.close()
 
     @property
-    def signals(self) -> Iterator[LslStreamSample]:
-        return self._signal_decoder
+    def signals(self) -> Generator[LslStreamSample, None, None]:
+        signal_decoder = LslStreamDecoder(self.filepath)
+        try:
+            for signal in signal_decoder:
+                yield signal
+        finally:
+            signal_decoder.close()
 
     @property
-    def input_actions(self) -> Iterator[InputAction]:
-        return self._input_action_decoder
+    def input_actions(self) -> Generator[InputAction, None, None]:
+        input_action_decoder = InputActionDecoder(self.filepath)
+        try:
+            for input_action in input_action_decoder:
+                yield input_action
+        finally:
+            input_action_decoder.close()
